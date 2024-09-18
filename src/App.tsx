175ef2f9a749ed236be581c56cc1fc9c022b8e40
alandanielvp <alanvp97@hotmail.com>
2024-09-18 13:00:02 -0600

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Box } from "@mui/material";
+import dario from "./images/dario.jpg"; // Make sure the image is in the correct path
+import { useEffect, useState } from "react";
+
+type TimeLeft = {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState<TimeLeft>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  function calculateTimeLeft(): TimeLeft {
+    const targetDate = new Date("2024-09-20T00:00:00");
+    const now = new Date();
+    const difference = targetDate.getTime() - now.getTime();
+
+    if (difference > 0) {
+      return {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    } else {
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      width={"100%"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      sx={{ backgroundColor: "gray" }}
+      height={"100vh"}
+    >
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <h1>Tiempo restante hasta su publicación:</h1>
+        {timeLeft.days !== undefined ? (
+          <h2>
+            {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m{" "}
+            {timeLeft.seconds}s
+          </h2>
+        ) : (
+          <h2>Ya es 20 de septiembre</h2>
+        )}
+      </Box>
+      <Box>
+        <img src={dario} width={500} alt="Dario" />
+      </Box>
+    </Box>
   );
 }
 
